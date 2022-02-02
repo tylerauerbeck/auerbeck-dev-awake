45d1ebababcd9b7c7a056e05c7a5d1ec950ea6ba
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2022-02-02 03:27:38 -0500

    errrrr

--- components/SiteHero.vue
+++ components/SiteHero.vue
@@ -1,5 +1,7 @@
 <template>
-  <section :class="front `hero is-medium hero-theme-${computedTheme}`">
+  <section 
+    class="front hero is-medium hero-theme-dark"
+  >
     <!-- Comment
     <img
       class="hero-bg-img"
